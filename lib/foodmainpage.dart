f1b21f6fb6a81a07d87e24d84ab4b1d26b4ad060
furknn <fs.2838@gmail.com>
2020-08-29 22:25:04 +0300

    added list view to bottom

--- lib/foodmainpage.dart
+++ lib/foodmainpage.dart
@@ -21,44 +21,67 @@ class FoodViewMainState extends State<FoodViewMain> {
     );
   }
 
-  Column bottomColumn() => Column(children: [row3Builder()]);
+  Column bottomColumn() =>
+      Column(children: [row3Builder(), bottomListBuilder()]);
 
-  Row row3Builder() => Row(
-        children: [
-          Container(
-            margin: EdgeInsets.fromLTRB(30, 30, 0, 0),
-            height: MediaQuery.of(context).size.height * 0.075,
-            width: MediaQuery.of(context).size.width * 0.1,
-            color: Colors.red,
-            child: Icon(
-              Icons.favorite,
-              color: Colors.white,
+  SizedBox bottomListBuilder() {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: ListTile(
+              isThreeLine: false,
+              leading: Container(
+                height: MediaQuery.of(context).size.height * 0.05,
+                width: MediaQuery.of(context).size.width * 0.1,
+                color: Colors.blue,
+              ),
             ),
+          );
+        },
+      ),
+    );
+  }
+
+  Container row3Builder() => Container(
+          child: ListTile(
+        leading: Container(
+          height: MediaQuery.of(context).size.height * 0.1,
+          width: MediaQuery.of(context).size.width * 0.1,
+          color: Colors.red,
+          child: Icon(
+            Icons.favorite,
+            color: Colors.white,
           ),
-          Container(
-              margin: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Popular",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ))
-        ],
-      );
+        ),
+        title: Text(
+          "Popular",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text("Popular Items"),
+        trailing: IconButton(
+          icon: Icon(Icons.arrow_forward),
+          onPressed: null,
+        ),
+      ));
 
   SizedBox row2Builder() => SizedBox(
         height: MediaQuery.of(context).size.height * 0.3,
         child: ListView.builder(
-          itemCount: 100,
+          itemCount: 20,
           scrollDirection: Axis.horizontal,
           itemBuilder: (BuildContext context, int index) {
             return Card(
-                margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
+                margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
                 child: Container(
                   width: MediaQuery.of(context).size.width * 0.4,
                   child: Column(
                     children: [
-                      Expanded(flex: 2, child: Placeholder()),
+                      Expanded(flex: 7, child: Placeholder()),
                       Expanded(
-                          flex: 1,
+                          flex: 4,
                           child: ListTile(
                               title: Text("Sweets"),
                               subtitle: Text("12 items")))
